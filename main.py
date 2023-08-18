4582e37c050a318bb0bfdde5e29282cdf2892e7e
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-08-18 21:53:30 +0800

    Make it run on Flask

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/hobbies")
+def hobbies():
+    return render_template("hobbies.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
